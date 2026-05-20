3b7931864f217b0b90c29593ea6731589bcf6300
Ekki-TS <trungsonle32@gmail.com>
2026-05-20 12:20:23 +0700

    Update b4.py

--- b4.py
+++ b4.py
@@ -1,16 +1,11 @@
 print("--- HỆ THỐNG KHAI BÁO NHÂN SỰ MỚI ---")
 
-while True:
-    employee_count = int(
-        input("Vui lòng nhập số lượng nhân sự mới trong tháng này: ")
-    )
+employee_count = 0
 
+while employee_count <= 0:
+    employee_count = int(input("Vui lòng nhập số lượng nhân sự mới trong tháng này: "))
     if employee_count > 0:
-        print(
-            f"[THÀNH CÔNG] Đã ghi nhận yêu cầu cấp phát tài sản cho " f"{employee_count} nhân sự mới!"
-        )
+        print(f"[THÀNH CÔNG] Đã ghi nhận yêu cầu cấp phát tài sản cho "f"{employee_count} nhân sự mới!")
         print("--- CHƯƠNG TRÌNH KẾT THÚC --- ")
-        break
     else:
-        print(
-            "[LỖI] Số lượng không hợp lệ! " "Vui lòng nhập một con số lớn hơn 0.")
+        print("[LỖI] Số lượng không hợp lệ! ""Vui lòng nhập một con số lớn hơn 0.")
